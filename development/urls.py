9afd4f5be94daca8d5518c1a9bbee7b2be612773
shamimhcp1 <shamimhcp1@gmail.com>
2023-12-19 01:59:11 +0600

    almost finished fronted design with login/register

--- development/urls.py
+++ development/urls.py
@@ -2,23 +2,42 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('', views.index, name='index'),
-    path('manage-users', views.manage_users, name='manage-users'),
-    path('manage-reports', views.manage_reports, name='manage-reports'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("forgot-password", views.forgot_password, name="forgot-password"), 
+    
+    # buyer
     path('buyer-add', views.buyer_add, name='buyer-add'),
     path('buyer-manage', views.buyer_manage, name='buyer-manage'),
+
+    # requirements
+    path('manage-requirements', views.manage_requirements, name='manage-requirements'),
+    path('add-requirement', views.add_requirement, name='add-requirement'),
     
-    path('dev-report/create', views.dev_report_create, name='dev-report-create'),
+    # report
+    path('create-report', views.create_report, name='create-report'),
+    path('manage-reports', views.manage_reports, name='manage-reports'),
     
+    # users
+    path('manage-users', views.manage_users, name='manage-users'),
+    path('add-user', views.add_user, name='add-user'),
+    
+    # profile
+    path('my-profile', views.my_profile, name='my-profile'),
+    path('change-password', views.change_password, name='change-password'),
+    
+    # pdf / Excel
     path('pdf-template', views.pdf_template, name='pdf-template'),
     path('pdf-view', views.pdf_view, name='pdf-view'),
     path('pdf-download', views.pdf_download, name='pdf-download'),
 
+    # API
     path('api/', views.ApiOverview, name='api-home'),
     path('api/buyers/', views.buyer_list_create_view, name='api-buyer-list-create'),
     path('api/buyers/<int:pk>/', views.buyer_detail_view, name='api-buyer-detail'),
     path('api/reports/', views.dev_report_list_view, name='api-dev-report-list'),
     path('api/reports/<int:pk>/', views.dev_report_detail_view, name='api-dev-report-detail'),
-    path('api/create/', views.dev_report_create, name='api-dev-report-create')
+    path('api/create/', views.create_report, name='api-dev-report-create')
 ]
--- development/views.py
+++ development/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from .models import Buyer, DevReport
+from .models import Buyer, DevReport, User
 from .serializers import BuyerSerializer, DevReportSerializer
 from .forms import DevReportForm
 from .utils import render_to_pdf
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.db import IntegrityError
 
+
+@login_required(login_url='login')
 def index(request):
     return render(request, 'development/index.html')
 
@@ -23,6 +28,7 @@ data = {
     "website": "dennisivy.com",
 }
 
+@login_required(login_url='login')
 def pdf_download(request):
     pdf = render_to_pdf('development/report-details.html', data)
     if pdf:
@@ -33,10 +39,11 @@ def pdf_download(request):
     return HttpResponse("Error generating PDF", status=500)
 
 
+@login_required(login_url='login')
 def pdf_template(request):
     return render(request, 'development/report-details.html', data)
 
-
+@login_required(login_url='login')
 def pdf_view(request):
     pdf = render_to_pdf('development/report-details.html', data)
     if pdf:
@@ -45,30 +52,10 @@ def pdf_view(request):
     return HttpResponse("Error generating PDF", status=500)
 
 
-@api_view(['GET'])
-def ApiOverview(request):
-
-    api_urls = {
-        'home': '/',
-        'buyer_list_create_view': '/buyers',
-        'buyer_detail_view': '/buyers/pk',
-        'dev_report_list_view': '/reports',
-        'dev_report_detail_view': '/reports/pk',
-        'dev_report_create': '/create'
-    }
-    return Response(api_urls)
-
-# Report List
-@api_view(['GET'])
-def dev_report_list_view(request):
-    reports = DevReport.objects.all()
-    serializer = DevReportSerializer(reports, many=True)
-    return Response(serializer.data)
-
-
 # Report Create
 # @api_view(['POST'])
-def dev_report_create(request):
+@login_required(login_url='login')
+def create_report(request):
     if request.method == 'POST':
         form = DevReportForm(request.POST)
         if form.is_valid():
@@ -92,19 +79,70 @@ def dev_report_create(request):
 
     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def manage_users(request):
-    return render(request, 'development/manage-users.html')
 
-def manage_reports(request):
-    return render(request, 'development/manage-reports.html')
-
+# buyer
+@login_required(login_url='login')
 def buyer_add(request):
     return render(request, 'development/buyer-add.html')
 
+@login_required(login_url='login')
 def buyer_manage(request):
     return render(request, 'development/buyer-manage.html')
 
+# requirements
+@login_required(login_url='login')
+def add_requirement(request):
+    return render(request, 'development/add-requirement.html')
 
+@login_required(login_url='login')
+def manage_requirements(request):
+    return render(request, 'development/manage-requirements.html')
+
+
+# reports
+@login_required(login_url='login')
+def manage_reports(request):
+    return render(request, 'development/manage-reports.html')
+
+# profile
+@login_required(login_url='login')
+def my_profile(request):
+    return render(request, 'development/my-profile.html')
+
+@login_required(login_url='login')
+def change_password(request):
+    return render(request, 'development/change-password.html')
+
+# users
+@login_required(login_url='login')
+def manage_users(request):
+    return render(request, 'development/manage-users.html')
+
+@login_required(login_url='login')
+def add_user(request):
+    return render(request, 'development/add-user.html')
+
+
+
+@api_view(['GET'])
+def ApiOverview(request):
+
+    api_urls = {
+        'home': '/',
+        'buyer_list_create_view': '/buyers',
+        'buyer_detail_view': '/buyers/pk',
+        'dev_report_list_view': '/reports',
+        'dev_report_detail_view': '/reports/pk',
+        'dev_report_create': '/create'
+    }
+    return Response(api_urls)
+
+# Report List
+@api_view(['GET'])
+def dev_report_list_view(request):
+    reports = DevReport.objects.all()
+    serializer = DevReportSerializer(reports, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def dev_report_detail_view(request, pk):
@@ -130,8 +168,6 @@ def dev_report_detail_view(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Buyer list
-
-
 @api_view(['GET', 'POST'])
 def buyer_list_create_view(request):
     if request.method == 'GET':
@@ -153,8 +189,6 @@ def buyer_list_create_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Buyer detail
-
-
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def buyer_detail_view(request, pk):
     try:
@@ -177,3 +211,68 @@ def buyer_detail_view(request, pk):
     elif request.method == 'DELETE':
         buyer.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "development/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "development/login.html")
+    
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "development/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "development/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "development/register.html")
+
+
+def forgot_password(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+        
+    return render(request, 'development/forgot-password.html')
